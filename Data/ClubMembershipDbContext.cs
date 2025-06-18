7995565dbeed8afc481c05d3fea0474d734cf8ec
Januario-Rose <januariorose3@gmail.com>
2025-06-18 06:15:03 +0200

    Add project files.

---
+++ Data/ClubMembershipDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ClubMembershipApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Data
+{
+    public class ClubMembershipDbContext:DbContext
+    {
+        static ClubMembershipDbContext()
+        {
+            SQLitePCL.Batteries.Init();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={AppDomain.CurrentDomain.BaseDirectory}ClubMembershipDb.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public required string EmailAddress { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string AddressFirstLine { get; set; }
+        public required string AddressSecondLine { get; set; }
+        public required string AddressCity { get; set; }
+        public required string PostCode { get; set; }
+    }
+}
